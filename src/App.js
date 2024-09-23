c344995309097a80e4f2a07efa1c06560bd6b311
sanhernandezmon <elsantimontano@gmail.com>
2024-09-22 19:48:58 -0500

    feature federico hizo un cambio

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Se hizo otro cambio
+          Se hizo otro cambio por federico
         </a>
       </header>
     </div>
