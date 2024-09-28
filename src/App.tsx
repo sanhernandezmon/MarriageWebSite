18e9398304f80872e1be2c5acbb9e934427bb805
sanhernandezmon <elsantimontano@gmail.com>
2024-09-28 17:55:19 -0500

    start proyect :tada:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.tsx
+import React from "react";
+import DayCounter from "./components/DayCounter";
+import Gallery from "./components/Gallery";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DayCounter />
+      <Gallery />
     </div>
   );
-}
+};
 
 export default App;
