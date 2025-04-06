e1ab99a563d30286798f2902794b829b75627751
Santiago Hernandez <elsantimontano@gmail.com>
2025-04-05 21:43:58 -0500

    feat lluvia de sobre 👔

--- src/components/welcome-area/index.tsx
+++ src/components/welcome-area/index.tsx
@@ -29,8 +29,9 @@ const Welcome = () => {
             <p><strong>Recepción:</strong> 5:00 pm; Finca El Refugio</p>
             <br></br>
             <h2>Este es un evento para adultos, solo los pajecitos escogidos nos acompañaran</h2>
-            <h2>Nuestro mayor regalo es tu presencia, pero si quieres ayudarnos a empezar esta nueva aventura... </h2>
-            <h2><strong>Prepárate para la lluvia de sobres </strong></h2>
+            <br></br>
+            <h2><strong>Nuestro mayor regalo es tu presencia, pero si quieres ayudarnos a empezar esta nueva aventura...</strong> </h2>
+            <h2><strong>¡Prepárate para la lluvia de sobres!</strong></h2>
           </div>
         </div>
 
